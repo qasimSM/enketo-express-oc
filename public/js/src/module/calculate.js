7cc0483da1190085075251f1c2dfbd3b7c19d6b3
Martijn van de Rijdt <martijn@enketo.org>
2021-12-15 16:33:05 -0500

    fixed: OC: a setvalue target without a form control causes an exception, #508

--- public/js/src/module/calculate.js
+++ public/js/src/module/calculate.js
@@ -7,7 +7,7 @@ calculationModule._originalUpdateCalc = calculationModule._updateCalc;
 calculationModule._updateCalc = function( control, props, emptyNonRelevant ){
     // OC customization:
     // Always clear if not relevant except if the setvalue target is a visible form control and is user-editable.
-    emptyNonRelevant = !( props.type === 'setvalue' && control.closest( '.question' ) && !control.matches( '[readonly]' ) );
+    emptyNonRelevant = !( props.type === 'setvalue' && control && control.closest( '.question' ) && !control.matches( '[readonly]' ) );
 
     return this._originalUpdateCalc( control, props, emptyNonRelevant );
 };
