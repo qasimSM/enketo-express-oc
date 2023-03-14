a4fe076abe200428a961add9a1c21455827438e0
Martijn van de Rijdt <martijn@enketo.org>
2023-03-14 10:00:34 -0400

    fixed: do not show 'saving is disabled' on form previews, #647

--- public/js/src/module/field-submission-queue.js
+++ public/js/src/module/field-submission-queue.js
@@ -22,7 +22,7 @@ const FIELDSUBMISSION_COMPLETE_URL = settings.enketoId
     : null;
 
 class FieldSubmissionQueue {
-    constructor() {
+    constructor(options = { showStatus: true }) {
         this.submissionQueue = {};
         this.submissionOngoing = null;
         this.lastAdded = {};
@@ -63,19 +63,21 @@ class FieldSubmissionQueue {
                 }
             },
             update(status) {
-                this.statusElements.forEach((element) => {
-                    element.classList.remove([
-                        'ongoing',
-                        'success',
-                        'error',
-                        'fail',
-                    ]);
-                    element.classList.add(status);
-                    element.dataset.i18n = `fieldsubmission.feedback.${status}`;
-                    element.textContent = t(
-                        `fieldsubmission.feedback.${status}`
-                    );
-                });
+                if (options.showStatus) {
+                    this.statusElements.forEach((element) => {
+                        element.classList.remove([
+                            'ongoing',
+                            'success',
+                            'error',
+                            'fail',
+                        ]);
+                        element.classList.add(status);
+                        element.dataset.i18n = `fieldsubmission.feedback.${status}`;
+                        element.textContent = t(
+                            `fieldsubmission.feedback.${status}`
+                        );
+                    });
+                }
             },
         };
 
