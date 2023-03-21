1cca98af274ac06e9791f3594d99caf123b5288f
Martijn van de Rijdt <martijn@enketo.org>
2023-03-21 11:17:18 -0400

    fixed: not able to run non-OC form views

--- public/js/src/enketo-webform.js
+++ public/js/src/enketo-webform.js
@@ -34,7 +34,6 @@ if (settings.offline) {
         .init(survey)
         .then(initTranslator)
         .then(formCache.init)
-        .then(_addBranding)
         .then(_swapTheme)
         .then(formCache.updateMaxSubmissionSize)
         .then(_updateMaxSizeSetting)
@@ -59,7 +58,6 @@ if (settings.offline) {
             })
         )
         .then(_swapTheme)
-        .then(_addBranding)
         .then(connection.getMaximumSubmissionSize)
         .then(_updateMaxSizeSetting)
         .then(_init)
@@ -163,29 +161,6 @@ function _setEmergencyHandlers() {
                 .catch(() => {});
         });
     }
-}
-
-/**
- * Adds/replaces branding if necessary, and unhides branding.
- *
- * @param { object } survey - [description]
- */
-function _addBranding(survey) {
-    const brandImg = document.querySelector('.form-header__branding img');
-    const attribute = settings.offline ? 'data-offline-src' : 'src';
-
-    if (
-        brandImg &&
-        survey.branding &&
-        survey.branding.source &&
-        brandImg.src !== survey.branding.source
-    ) {
-        brandImg.src = '';
-        brandImg.setAttribute(attribute, survey.branding.source);
-    }
-    brandImg.classList.remove('hide');
-
-    return survey;
 }
 
 /**
